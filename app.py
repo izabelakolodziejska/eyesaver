a0f9016f2a972145400310d6acfe817361a75f38
izabelakolodziejska <izabela.kol@protonmail.com>
2025-10-07 20:18:29 +0200

    start button

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 import sys
 
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QFont, QCursor
+from PyQt6.QtGui import QFont, QCursor, QBrush, QPixmap
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton
 
 app = QApplication(sys.argv)
 window = QWidget()
 window.setWindowTitle("eyesaver")
 window.setFixedSize(400, 250)
-#additional css to prevent image distortion
-window.setStyleSheet("""
-    background-image: url(background.jpg);
-    background-repeat: no-repeat;
-    background-position: center;
-""")
+#setStyleSheet messed with buttons (no button color was displayed)
+palette = window.palette()
+palette.setBrush(window.backgroundRole(), QBrush(QPixmap("background.jpg")))
+window.setPalette(palette)
 
 #vertical layout: title and two buttons
 layout = QVBoxLayout()
@@ -23,24 +21,25 @@ title = QLabel("eyesaver~")
 font = QFont('Sitka', 22)
 font.setBold(True)
 title.setFont(font)
-title.setStyleSheet("color: white; margin-top: 45px;")
+title.setStyleSheet("color: #fff9ed; margin-top: 45px;")
 title.setAlignment(Qt.AlignmentFlag.AlignHCenter)
 
-#to do: bigger font, no background color id displayed
 start = QPushButton("start")
 start.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
+start.setFixedSize(200, 50)
 start.setStyleSheet("""
     QPushButton {
-        background-color: #8B008B;         
-        color: white;                    
+        background-color: #bf87a3;         
+        color: #fff9ed;                    
         font-family: Sitka;                
-        font-size: 12pt;                   
+        font-size: 15pt;      
+        font-weight: bold;             
         border: none;                     
         border-radius: 10px;              
-        padding: 6px 12px;                
+        padding: 6px 12px;     
     }
     QPushButton:hover {
-        background-color: #9A32CD;        
+        background-color: #a06b85;        
     }
     QPushButton:pressed {
         background-color: #6A006A;         
@@ -48,7 +47,7 @@ start.setStyleSheet("""
 """)
 
 layout.addWidget(title)
-layout.addWidget(start)
+layout.addWidget(start, alignment=Qt.AlignmentFlag.AlignCenter)
 
 window.setLayout(layout)
 
